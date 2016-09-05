921f3ba56e743b3a5d060027bdf1db2d683ad8f9
changwoo <ep6tri@hotmail.com>
2016-09-06 03:55:55 +0900

    4.3 setter 인젝션 이용하기
    setter 인젝션을 이용한 의존성 처리를 구현.
    또한 생성자를 이용해 처리하는 것도  context나 소스의 주석 처리된 부분을 살리면 가능도록 됨.

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,19 +1,48 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
-	public SamsungTV() {
-		System.out.println("===> SamsungTV) 객체 생성");
+	
+	private Speaker speaker;
+	private int price;
+	
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 호출");
+		this.speaker = speaker;
 	}
+
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 호출");
+		this.price = price;
+	}
+
+//	public SamsungTV() {
+//		System.out.println("===> SamsungTV(1) 객체 생성");
+//	}
+//	
+//	public SamsungTV(Speaker speaker) {
+//		System.out.println("===> SamsungTV(2) 객체 생성");
+//		this.speaker = speaker;
+//	}
+//	
+//	public SamsungTV(Speaker speaker, int price) {
+//		System.out.println("===> SamsungTV(2) 객체 생성");
+//		this.speaker = speaker;
+//		this.price = price;
+//	}
+	
 	public void powerOn() {
-		System.out.println("SamsungTV--전원 켠다.");
+		System.out.println("SamsungTV--전원 켠다. (가격: " + price + ")");
 	}
+	
 	public void powerOff() {
 		System.out.println("SamsungTV--전원 끈다.");
 	}
+	
 	public void volumeUp() {
-		System.out.println("SamsungTV--소리 올린다.");
+		this.speaker.volumeUp();
 	}
+	
 	public void volumeDown() {
-		System.out.println("SamsungTV--소리 내린다.");
+		this.speaker.volumeDown();
 	}
 }
