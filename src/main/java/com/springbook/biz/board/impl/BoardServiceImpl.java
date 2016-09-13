23fb7eed5a5d6ae6df715c4a013eaadcde596644
changwoo <ep6tri@hotmail.com>
2016-09-14 06:15:39 +0900

    Day 04 Class 03 실습
    중요한 개념을 실습한다.
    - 비즈니스 레이어와 프레젠테이션 레이어를 분리하는 과정이 진행된다.
    - ContextLoaderListener를 등록하여 applicationContext.xml을 읽는다.

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -12,7 +12,7 @@ import com.springbook.biz.board.BoardVO;
 public class BoardServiceImpl implements BoardService {
 
 	@Autowired
-	private BoardDAOSpring boardDAO;
+	private BoardDAO boardDAO;
 
 	@Override
 	public void insertBoard(BoardVO vo) {
--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -3,6 +3,7 @@ package com.springbook.view.board;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
@@ -18,9 +20,13 @@ import com.springbook.biz.board.impl.BoardDAO;
 @SessionAttributes("board")
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+
 	// 검색 조건 목록 설정
 	@ModelAttribute("conditionMap")
 	public Map<String, String> searchConditionMap() {
+
 		Map<String, String> conditionMap = new HashMap<String, String>();
 
 		conditionMap.put("제목", "TITLE");
@@ -29,54 +35,50 @@ public class BoardController {
 		return conditionMap;
 	}
 
+	// 글 등록
 	@RequestMapping(value="/insertBoard.do")
-	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String insertBoard(BoardVO vo) {
 
-		boardDAO.insertBoard(vo);
+		boardService.insertBoard(vo);
 
 		return "getBoardList.do";
 	}
 
+	// 글 수정
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
 
-		System.out.println("번호: " +  vo.getWriter());
-		System.out.println("제목: " +  vo.getTitle());
-		System.out.println("작성자: " +  vo.getWriter());
-		System.out.println("내용: " +  vo.getContent());
-		System.out.println("등록일: " +  vo.getRegDate());
-		System.out.println("조회수: " +  vo.getCnt());
-		boardDAO.updateBoard(vo);
+		boardService.updateBoard(vo);
 
 		return "getBoardList.do";
 	}
 
+	// 글 삭제
 	@RequestMapping("/deleteBoard.do")
-	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String deleteBoard(BoardVO vo) {
 
-		boardDAO.deleteBoard(vo);
+		boardService.deleteBoard(vo);
 
 		return "getBoardList.do";
 	}
 
+	// 글 상세 조회
 	@RequestMapping("/getBoard.do")
-	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
+	public String getBoard(BoardVO vo, Model model) {
 
-		model.addAttribute("board", boardDAO.getBoard(vo));
+		model.addAttribute("board", boardService.getBoard(vo));
 
 		return "getBoard.jsp";
 	}
 
+	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
 	public String getBoardList(
-			@RequestParam(value="searchCondition", defaultValue="TITLE", required=false) String condition,
-			@RequestParam(value="searchKeyword", defaultValue="", required=false) String keyword,
-			BoardVO vo, BoardDAO boardDAO, Model model) {
+			//@RequestParam(value="searchCondition", defaultValue="TITLE", required=false) String condition,
+			//@RequestParam(value="searchKeyword", defaultValue="", required=false) String keyword,
+			BoardVO vo, Model model) {
 
-		System.out.println("검색 조건: " + condition);
-		System.out.println("검색 단어: " + keyword);
-
-		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 
 		return "getBoardList.jsp";
 	}
